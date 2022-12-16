ee58d1815bc19230761e68c744f8977d25108522
User8916948 <buravlyov.d@gmail.com>
2022-12-16 01:22:23 +0100

    Button functions realised, without pdf format

--- Text Redactor/MainWindow.xaml.cs
+++ Text Redactor/MainWindow.xaml.cs
@@ -22,6 +22,9 @@ namespace Text_Redactor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string fileName = "text";
+        private bool saved = false;
+        private Encoding encoding = Encoding.UTF8;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,32 +32,132 @@ namespace Text_Redactor
 
         private void btNew_Click(object sender, RoutedEventArgs e)
         {
+            if (!saved)
+            {
+                MessageBoxResult result= MessageBox.Show("Do you want to save changes", "Text Redactor", MessageBoxButton.YesNoCancel);
+                {
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        if (File.Exists(fileName))
+                        {
+                            File.WriteAllText(fileName, new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd).Text, encoding);
+                            rtbText.Document.Blocks.Clear(); fileName = "text";
+                        }
+                        else
+                        {
+                            SaveFileDialog dlg = new SaveFileDialog();
 
+                            dlg.Filter = "All Files (*.*)|*.*|Text Files (*.txt)|*.txt|Pdf Files (*.pdf)|*.pdf|Rtf Files (*.rtf)|*.rtf";
+                            dlg.FilterIndex = 2;
+                            dlg.FileName = fileName;
+
+                            bool? res = dlg.ShowDialog();
+                            if (res.HasValue && res.Value)
+                            {
+                                File.WriteAllText(dlg.FileName, new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd).Text, encoding);
+                                rtbText.Document.Blocks.Clear();fileName = "text";
+                            }
+
+                        }
+                        
+                    }else if(result == MessageBoxResult.No)
+                    {
+                        rtbText.Document.Blocks.Clear();
+                    }
+                }
+
+            }
+            else
+            {
+                rtbText.Document.Blocks.Clear();
+                saved = false;
+            }
+            
+        }
+        void SaveToRtf(string _fileName)
+        {
+            TextRange range;
+            FileStream fStream;
+            range = new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd);
+            fStream = new FileStream(_fileName, FileMode.Create);
+            range.Save(fStream, DataFormats.Rtf);
+            fStream.Close();
         }
 
         private void btOpen_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            bool? res = openFileDialog.ShowDialog();
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //bool? res = openFileDialog.ShowDialog();
+            //if (res.HasValue && res.Value)
+            //{
+            //    using (FileStream fstream = new FileStream(openFileDialog.FileName, FileMode.Open))
+            //    {
+            //        TextRange range = new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd);
+            //        range.Load(fstream, DataFormats.XamlPackage);
+
+            //    }
+            //}
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "All Files (*.*)|*.*|Text Files (*.txt)|*.txt|Pdf Files (*.pdf)|*.pdf|Rtf Files (*.rtf)|*.rtf";
+            dlg.FilterIndex = 2;
+            bool? res = dlg.ShowDialog();
             if (res.HasValue && res.Value)
             {
-                using(FileStream fstream = new FileStream(openFileDialog.FileName, FileMode.Open))
+                rtbText.Document.Blocks.Clear();
+                using (StreamReader reader = File.OpenText(dlg.FileName))
                 {
-                    TextRange range = new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd);
-                    range.Load(fstream, DataFormats.XamlPackage);
-                    
+                    rtbText.Document.Blocks.Add(new Paragraph(new Run(reader.ReadToEnd())));
                 }
+                fileName = dlg.FileName;
+                saved = true;
             }
+            
         }
 
         private void btSave_Click(object sender, RoutedEventArgs e)
         {
+            if (File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd).Text, encoding);
+                saved = true;
+            }
+            else
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.Filter = "All Files (*.*)|*.*|Text Files (*.txt)|*.txt|Pdf Files (*.pdf)|*.pdf|Rtf Files (*.rtf)|*.rtf";
+                dlg.FilterIndex = 2;
+                dlg.FileName = fileName;
+                bool? res = dlg.ShowDialog();
+                if (res.HasValue && res.Value)
+                {
+                    File.WriteAllText(dlg.FileName, new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd).Text, encoding);
+                    saved = true;
+                }
+
+            }
 
         }
 
         private void btSaveas_Click(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog dlg = new SaveFileDialog();
 
+            dlg.Filter = "All Files (*.*)|*.*|Text Files (*.txt)|*.txt|Pdf Files (*.pdf)|*.pdf|Rtf Files (*.rtf)|*.rtf";
+            dlg.FilterIndex = 2;
+            dlg.FileName = fileName;
+            bool? res = dlg.ShowDialog();
+            if (res.HasValue && res.Value)
+            {
+                File.WriteAllText(dlg.FileName, new TextRange(rtbText.Document.ContentStart, rtbText.Document.ContentEnd).Text); 
+                saved = true;
+
+            }
+            
+        }
+
+        private void rtbText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            saved = false;
         }
     }
 }
